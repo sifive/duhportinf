fac726144197d6b206c4e39f15806c10dfd51c5f
alex bishara <abishara@cs.stanford.edu>
2019-02-01 16:40:07 -0800

    rename package to duhportinf

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='abxportinf',
+    name='duhportinf',
     version='0.1',
     description='library to match flat named port list to library of bus specifications',
     entry_points = {'console_scripts': ['abxportinf=abxportinf.main:main']},
-    url='https://github.com/abishara/wire-inference',
+    url='https://github.com/sifive/duhportinf',
     author='Alex Bishara',
     author_email='alex.bishara@sifive.com',
     license='MIT',
-    packages=['abxportinf'],
+    packages=['duhportinf'],
 )
