2b480a0bc6ea5821905ec2b268e4912cdc3b1c52
alex bishara <abishara@cs.stanford.edu>
2019-02-11 13:40:15 -0800

    changed .npm-install gen-local-req to use Loose instead of Strict version parser

--- .npm-install/gen-local-req.py
+++ .npm-install/gen-local-req.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 import sys
 import subprocess
-from distutils.version import StrictVersion as version
+from distutils.version import LooseVersion as version
 
 assert len(sys.argv) == 3
 inreq_path, outreq_path = sys.argv[1:]
 
 def libv(s):
-    s = str(s)
+    s = str(s).strip()
     return s.split('==') if '==' in s else s.split('>=')
 
 txt = subprocess.check_output(['pip3', 'list', '--format=freeze'])
@@ -15,16 +15,14 @@ txt = txt.decode('ascii', 'ignore')
 installed = {}
 for line in txt.split():
     lib, v = libv(line)
-    #installed[lib] = version(v)
-    installed[lib] = True
+    installed[lib] = version(v)
 with open(inreq_path) as fin, \
      open(outreq_path, 'w') as fout:
     for line in fin:
         rlib, rv = libv(line)
         if (
             rlib not in installed or
-            False
-            #version(rv) > installed[rlib]
+            version(rv) > installed[rlib]
         ):
             fout.write(line)
 
